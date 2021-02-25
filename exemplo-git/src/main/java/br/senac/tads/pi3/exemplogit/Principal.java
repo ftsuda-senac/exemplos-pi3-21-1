995bec8afd78cd557ecd2dc1d1ca5081a2b68c2c
ftsuda-senac <ftsuda.senac@gmail.com>
2021-02-25 10:48:26 -0300

    Entrada de dados

--- exemplo-git/src/main/java/br/senac/tads/pi3/exemplogit/Principal.java
+++ exemplo-git/src/main/java/br/senac/tads/pi3/exemplogit/Principal.java
@@ -5,6 +5,8 @@
  */
 package br.senac.tads.pi3.exemplogit;
 
+import java.util.Scanner;
+
 public class Principal {
 
     public static void main(String[] args) {
@@ -13,17 +15,21 @@ public class Principal {
         System.out.println("Projeto exemplo-git");
         System.out.println("================");
         
-        System.out.println("Teste");
         
         int i = 1;
         int j = 1;
         System.out.println("i++ = " + i++);
         System.out.println("++j = " + ++j);
-        System.out.println("Teste de acesso Alexsandro");
-        System.out.println("Tabuada do 9");
+        
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite a tabuada que será calculada: ");
+        String val = entrada.nextLine();
+        
+        int tab = Integer.parseInt(val);
+        System.out.println("Tabuada do " + tab);
         for (int k = 1; k < 11; k++) {
-            System.out.println("9 x " + k + " = " + k*9);
-            
+            System.out.println("" + tab + " x " + k + " = " + tab*k);
+
             System.out.println("Alteração 2 - Alexsandro");
         }
     }
