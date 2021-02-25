2cb59e313c4f3e4ac8cbd4c44c17f3db1b550fa2
ftsuda-senac <ftsuda.senac@gmail.com>
2021-02-25 10:52:37 -0300

    Correção posição da mensagem

--- exemplo-git/src/main/java/br/senac/tads/pi3/exemplogit/Principal.java
+++ exemplo-git/src/main/java/br/senac/tads/pi3/exemplogit/Principal.java
@@ -23,9 +23,8 @@ public class Principal {
         System.out.println("Tabuada do 9");
         for (int k = 1; k < 11; k++) {
             System.out.println("9 x " + k + " = " + k*9);
-            
-            System.out.println("Alteração 2 - Alexsandro");
         }
+        System.out.println("Alteração 2 - Alexsandro");
     }
     
 }
