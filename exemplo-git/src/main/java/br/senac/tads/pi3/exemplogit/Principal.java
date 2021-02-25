b0d1cc16d0104ea5020f2e13e2a6f087d88d2a8a
ftsuda-senac <ftsuda.senac@gmail.com>
2021-02-25 09:52:03 -0300

    Alteração na lógica

--- exemplo-git/src/main/java/br/senac/tads/pi3/exemplogit/Principal.java
+++ exemplo-git/src/main/java/br/senac/tads/pi3/exemplogit/Principal.java
@@ -9,7 +9,14 @@ public class Principal {
 
     public static void main(String[] args) {
         
+        System.out.println("================");
         System.out.println("Projeto exemplo-git");
+        System.out.println("================");
+        
+        int i = 1;
+        int j = 1;
+        System.out.println("i++ = " + i++);
+        System.out.println("++j = " + ++j);
     }
     
 }
