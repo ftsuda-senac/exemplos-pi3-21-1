7ae363e8a6ac8ca30f271001749f330977372944
ftsuda-senac <ftsuda.senac@gmail.com>
2021-03-25 11:30:58 -0300

    Exemplo web com Servlet e JSP

---
+++ exemplo-web/src/main/java/br/senac/tads/pi3/exemploweb/ExemploServlet.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senac.tads.pi3.exemploweb;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "ExemploServlet", urlPatterns = "/exemplo-servlet")
+public class ExemploServlet extends HttpServlet {
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html");
+
+        try (PrintWriter out = response.getWriter()) {
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Exemplo Servlet</title>");
+            out.println("<meta charset=\"UTF-8\" />");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Exemplo Servlet</h1>");
+            out.println("<p>Data e hora atual: " + LocalDateTime.now() + "</p>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+    }
+}
